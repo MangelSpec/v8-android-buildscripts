9304158af6180eeeb502db47d83c1ec4d2bd7667
Mona Lisa <Mona Lisa>
2023-09-13 11:26:47 +0100

    mono

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,7 @@ ARCH=$2
 
 GN_ARGS_BASE="
   v8_static_library=true
+  v8_monolithic = true
   is_component_build=false
   use_custom_libcxx=false
   icu_use_data_file=false
@@ -149,9 +150,15 @@ function copyLib()
   local target_ext=''
   local lib_dir=''
   if [[ ${PLATFORM} = "android" ]]; then
-    target="v8_monolith"
+    target="libv8_libbase"
     target_ext=".a"
     lib_dir="${platform_arch}"
+
+    mkdir -p "${BUILD_DIR}/lib/${lib_dir}"
+
+    cp -f "${output_dir}/libv8_libbase.a" "${BUILD_DIR}/lib/${lib_dir}/libv8_libbase.a"
+    cp -f "${output_dir}/libv8_libplatform.a" "${BUILD_DIR}/lib/${lib_dir}/libv8_libplatform.a"
+    cp -f "${output_dir}/libv8_monolith.a" "${BUILD_DIR}/lib/${lib_dir}/libv8_monolith.a"
   elif [[ ${PLATFORM} = "ios" ]]; then
     target="libv8"
     target_ext=".dylib"
@@ -160,13 +167,13 @@ function copyLib()
     exit 1
   fi
 
-  mkdir -p "${BUILD_DIR}/lib/${lib_dir}"
-  cp -f "${output_dir}/${target}${target_ext}" "${BUILD_DIR}/lib/${lib_dir}/${target}${target_ext}"
+  # mkdir -p "${BUILD_DIR}/lib/${lib_dir}"
+  # cp -f "${output_dir}/${target}${target_ext}" "${BUILD_DIR}/lib/${lib_dir}/${target}${target_ext}"
 
-  if [[ -d "${output_dir}/lib.unstripped" ]]; then
-    mkdir -p "${BUILD_DIR}/lib.unstripped/${lib_dir}"
-    cp -f "${output_dir}/lib.unstripped/${target}${target_ext}" "${BUILD_DIR}/lib.unstripped/${lib_dir}/${target}${target_ext}"
-  fi
+  # if [[ -d "${output_dir}/lib.unstripped" ]]; then
+  #   mkdir -p "${BUILD_DIR}/lib.unstripped/${lib_dir}"
+  #   cp -f "${output_dir}/lib.unstripped/${target}${target_ext}" "${BUILD_DIR}/lib.unstripped/${lib_dir}/${target}${target_ext}"
+  # fi
 }
 
 function copySnapshot()
